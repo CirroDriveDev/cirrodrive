4ef5890d601fe9c948001bfa222fcb552b9cde9c
ZetabS <songkm2@naver.com>
2024-10-08 11:02:16 +0900

    fix: 백엔드 predev 스크립트 수정

--- apps/backend/predev.ts
+++ apps/backend/predev.ts
@@ -1,57 +1,80 @@
-import { exec } from "node:child_process";
+import { execSync } from "node:child_process";
+
+const Console = {
+  log: (message?: string) => {
+    process.stdout.write(`${message ?? ""}\n`);
+  },
+
+  error: (message?: string) => {
+    process.stderr.write(`${message ?? ""}\n`);
+  },
+
+  write: (message: string) => {
+    process.stdout.write(message);
+  },
+};
 
 // 컨테이너 이름
 const containerName = "database-dev";
 
 // 컨테이너 상태를 확인하는 함수
-function checkContainerHealth(): Promise<boolean> {
-  return new Promise((resolve, reject) => {
-    exec(
-      `docker inspect -f {{.State.Health.Status}} ${containerName}`,
-      (error, stdout, stderr) => {
-        if (error) {
-          if (error.code === 127) {
-            resolve(true);
-            return;
-          }
-          reject(error);
-          return;
-        }
-        if (stderr) {
-          reject(new Error(stderr));
-          return;
-        }
+function checkContainerHealth(): boolean {
+  const healthStatus = execSync(
+    `docker inspect -f {{.State.Health.Status}} ${containerName}`,
+  )
+    .toString()
+    .trim();
 
-        const healthStatus = stdout.trim();
-        resolve(healthStatus === "healthy");
-      },
-    );
-  });
+  return healthStatus === "healthy";
 }
 
 // 1초 간격으로 컨테이너 상태를 확인하는 함수
 async function waitForContainerHealth(): Promise<void> {
+  const MAX_COUNT = 30;
   let healthy = false;
-  process.stdout.write("MariaDB 로딩.");
+  let count = 1;
+  Console.write("[MariaDB] 컨테이너 상태 확인 중...");
 
   while (!healthy) {
-    healthy = await checkContainerHealth();
+    healthy = checkContainerHealth();
     if (!healthy) {
-      process.stdout.write(".");
+      Console.log(" 실패");
+      if (count > MAX_COUNT) {
+        Console.error("[MariaDB] 컨테이너 상태 확인 실패");
+        process.exit(1);
+      }
       await new Promise<void>((resolve) => {
         setTimeout(resolve, 1000);
       });
+      Console.write(`[MariaDB] 재시도 ${(count++).toString()}회...`);
+    } else {
+      Console.log(" 성공");
     }
   }
-  process.stdout.write("\n");
-  process.stdout.write("MariaDB 로딩 완료\n");
 }
 
-function main(): void {
-  exec("npm run compose:dev:up -- database-dev");
-  void waitForContainerHealth();
-  exec("prisma db push");
+async function main(): Promise<void> {
+  try {
+    execSync("docker --version");
+
+    Console.write("[MariaDB] 실행 중인 개발 서버 종료...");
+    execSync("npm run compose:dev:down -- database-dev", { stdio: "ignore" });
+    Console.log(" 성공");
+
+    Console.write("[MariaDB] 개발 서버 실행...");
+    execSync("npm run compose:dev:up -- database-dev", { stdio: "ignore" });
+    Console.log(" 성공");
+
+    await waitForContainerHealth();
+    Console.log();
+  } catch {
+    Console.log("Docker가 설치되어 있지 않습니다.\nMariaDB 실행을 건너뜁니다.");
+  }
+
+  Console.log("[Prisma] db push 실행");
+  execSync("prisma db push", { stdio: "inherit" });
+  Console.log("[Prisma] 데이터베이스에 스키마 동기화 완료");
 }
 
 // 스크립트 실행
-main();
+void main();
