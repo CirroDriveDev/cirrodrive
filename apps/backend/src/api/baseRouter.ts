eef115e765be8c100c0d8c7dee0754e44d711446
lirafy <lirafy908@gmail.com>
2024-10-22 12:56:54 +0900

    code-api

--- apps/backend/src/api/baseRouter.ts
+++ apps/backend/src/api/baseRouter.ts
@@ -1,7 +1,14 @@
+// src/api/routes/baseRouter.ts
 import { Router } from "express";
+import { FileRouter } from "@/api/fileRouter.ts";
+import { codeRouter } from "@/api/codeRouter.ts";
 
 export const BaseRouter = (): Router => {
   const router = Router();
+
+  // 파일 및 코드 관련 라우터
+  router.use("/files", FileRouter());
+  router.use("/codes", codeRouter());
 
   return router;
 };
