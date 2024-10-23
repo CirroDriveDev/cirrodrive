f4d9e0ff95dd344117f2cc1f1ca3093851752222
Bear-moon <post120283@gmail.com>
2024-10-24 03:01:21 +0900

    feat: CodePage 추가 (#28)
    * design: UploadPage ui변경
    
    연결x
    ui만 있음
    
    내 PC버튼(작동x)
    Drag and Drop (X)
    
    추가 수정 예정 있음
    
    * feat: CodePage 추가
    
    CodePage에는 업로드한 파일의 이름과 코드, 남은 날짜를 보여주는 페이지입니다
    
    UploadPage의 Code 버튼을 누르면 작은 창으로 나옵니다
    
    CodePage를 열기 위해 UploadPage의 코드도 살짝 변경되었습니다

--- apps/frontend/src/pages/upload/ui/UploadPage.tsx
+++ apps/frontend/src/pages/upload/ui/UploadPage.tsx
@@ -1,10 +1,49 @@
+import { File } from "lucide-react";
 import { WorkspaceLayout } from "@/widgets/WorkspaceLayout/ui/WorkspaceLayout.tsx";
 
 export function UploadPage(): JSX.Element {
   return (
     <WorkspaceLayout>
-      <div className="flex flex-grow items-center justify-center">
-        <div>업로드 페이지입니다</div>
+      <div className="w-full">
+        <div className="mt-3 flex flex-row text-gray-300">
+          <div className="ml-20 h-[30px] w-[100px] text-center align-middle">
+            Name
+          </div>
+          <div className="ml-[500px] h-[30px] w-[100px] text-center align-middle">
+            Size
+          </div>
+          <div className="ml-[200px] h-[30px] w-[100px] text-center align-middle">
+            Actions
+          </div>
+        </div>
+        <div className="flex h-[520px] items-center justify-center">
+          <div className="bg-gray-50">
+            <File size={60} color="gray" />
+          </div>
+        </div>
+        <form action="#" method="post" encType="multipart/form-data">
+          <div className="relative flex flex-row bg-gray-400">
+            <span className="absolute right-0 top-0">
+              <button type="submit" className="w-[100px] bg-gray-300">
+                업로드
+              </button>
+            </span>
+            <div className="w-[90px] bg-gray-300">
+              <button
+                type="button"
+                className="ms-6"
+                onClick={() => {
+                  window.open("/Code", "width=800,height=400", "_self");
+                }}
+              >
+                코드
+              </button>
+            </div>
+          </div>
+          <div className="place-self-center">
+            <input type="file" name="#" className="mt-20" />
+          </div>
+        </form>
       </div>
     </WorkspaceLayout>
   );
