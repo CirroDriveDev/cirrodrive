56ec7eba6b2088b9f050c5d97ea95f105273559c
ZetabS <songkm2@naver.com>
2024-11-02 23:19:34 +0900

    feat: 프론트엔드 tRPC 마이그레이션

---
+++ apps/frontend/src/app/provider/trpcClient.ts
@@ -0,0 +1,16 @@
+import { httpBatchLink } from "@trpc/client";
+import { SuperJSON } from "superjson";
+import { trpc } from "@/shared/api/trpc.ts";
+
+const VITE_TRPC_SERVER_URL = import.meta.env.VITE_TRPC_SERVER_URL;
+const VITE_TRPC_SERVER_PORT = import.meta.env.VITE_TRPC_SERVER_PORT;
+const TRPC_PATH = "/trpc";
+
+export const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: `${VITE_TRPC_SERVER_URL}:${VITE_TRPC_SERVER_PORT}${TRPC_PATH}`,
+      transformer: SuperJSON,
+    }),
+  ],
+});
