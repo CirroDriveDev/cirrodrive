7952a128499d2c6b4c56ff53dbfea1f4caf42040
ZetabS <songkm2@naver.com>
2024-11-03 00:03:50 +0900

    chore: 린터와 포맷터 설정 수정 (#38)
    * chore: eslint 설정 수정
    
    * chore: prettier tsdoc 지원
    
    * chore: 잡다한 변경사항
    
    * chore: lintstaged 설정 변경

--- tooling/eslint/configs/_base.js
+++ tooling/eslint/configs/_base.js
@@ -1,18 +1,14 @@
 import eslint from "@eslint/js";
 import importPlugin from "eslint-plugin-import";
-import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 
 import rules from "../rules/index.js";
 
 export default tseslint.config(
   {
-    plugins: {
-      import: importPlugin,
-      turbo: turboPlugin,
-    },
     extends: [
       eslint.configs.recommended,
+      importPlugin.flatConfigs.recommended,
       rules.bestPractice,
       rules.comments,
       rules.es6,
@@ -22,19 +18,6 @@ export default tseslint.config(
       rules.unicorn,
       rules.variables,
     ],
-    rules: {
-      ...turboPlugin.configs.recommended.rules,
-      "import/extensions": ["error", "ignorePackages"],
-      "unicorn/filename-case": [
-        "error",
-        {
-          cases: {
-            camelCase: true,
-            pascalCase: true,
-          },
-        },
-      ],
-    },
   },
   {
     linterOptions: {
--- tooling/prettier/index.js
+++ tooling/prettier/index.js
@@ -1,15 +1,24 @@
 import { fileURLToPath } from "url";
 
-/** @typedef {import("prettier").Config} PrettierConfig */
-/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+/**
+ * @typedef {import("prettier").Config} PrettierConfig
+ */
+/**
+ * @typedef {import("prettier-plugin-jsdoc").AllOptions} JSDocConfig
+ */
+/**
+ * @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig
+ */
 
-/** @type { PrettierConfig | TailwindConfig } */
+/**
+ * @type {PrettierConfig | JSDocConfig | TailwindConfig}
+ */
 const config = {
   arrowParens: "always",
   bracketSameLine: false,
   bracketSpacing: true,
   embeddedLanguageFormatting: "auto",
-  endOfLine: "auto",
+  endOfLine: "lf",
   experimentalTernaries: true,
   htmlWhitespaceSensitivity: "css",
   insertPragma: false,
@@ -24,7 +33,26 @@ const config = {
   tabWidth: 2,
   trailingComma: "all",
   useTabs: false,
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: ["prettier-plugin-jsdoc", "prettier-plugin-tailwindcss"],
+
+  // prettier-plugin-jsdoc
+  tsdoc: true,
+  // jsdocAddDefaultToDescription: false,
+  // jsdocCapitalizeDescription: false,
+  jsdocCommentLineStrategy: "multiline",
+  // jsdocDescriptionTag: true,
+  // jsdocDescriptionWithDot: true,
+  // jsdocKeepUnParseAbleExampleIndent: true,
+  // jsdocLineWrappingStyle: "greedy",
+  // jsdocPreferCodeFences: true,
+  // jsdocPrintWidth: undefined,
+  // jsdocSeparateReturnsFromParam: true,
+  // jsdocSeparateTagGroups: true,
+  // jsdocSpaces: 1,
+  // jsdocTagsOrder: "undefined",
+  // jsdocVerticalAlignment: true,
+
+  // prettier-plugin-tailwindcss
   tailwindConfig: fileURLToPath(
     new URL("../../tooling/tailwind/web.ts", import.meta.url),
   ),
