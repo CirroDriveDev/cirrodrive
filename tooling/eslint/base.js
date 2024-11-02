cf8516b0539cd889cc5e5a87e87bdb173af14592
ZetabS <songkm2@naver.com>
2024-11-03 00:03:50 +0900

    chore: 린터와 포맷터 설정 수정 (#38)
    * chore: eslint 설정 수정
    
    * chore: prettier tsdoc 지원
    
    * chore: 잡다한 변경사항
    
    * chore: lintstaged 설정 변경

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -4,12 +4,31 @@ import baseConfig from "./configs/_base.js";
 import typescriptConfig from "./configs/typescript.js";
 import vitestConfig from "./configs/vitest.js";
 
+import turboPlugin from "eslint-plugin-turbo";
 export default tseslint.config(
   {
     ignores: ["dist/**"],
   },
   {
     extends: [...baseConfig, ...typescriptConfig, ...vitestConfig],
+  },
+  {
+    plugins: {
+      turbo: turboPlugin,
+    },
+    rules: {
+      ...turboPlugin.configs.recommended.rules,
+      "import/extensions": ["error", "ignorePackages"],
+      "unicorn/filename-case": [
+        "error",
+        {
+          cases: {
+            camelCase: true,
+            pascalCase: true,
+          },
+        },
+      ],
+    },
   },
   {
     files: ["**/*.js"],
