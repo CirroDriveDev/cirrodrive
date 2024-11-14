5fceb3eb6b5c66218c5dd3bab418a24c5b2a667a
ZetabS <songkm2@naver.com>
2024-11-14 14:33:37 +0900

    fix: userschema (#51)

--- apps/frontend/src/pages/login/api/useLogin.ts
+++ apps/frontend/src/pages/login/api/useLogin.ts
@@ -3,7 +3,7 @@ import type { AppRouter, RouterInput, RouterOutput } from "@cirrodrive/backend";
 import type { UseTRPCMutationOptions } from "@trpc/react-query/shared";
 import type { TRPCClientErrorLike } from "@trpc/client";
 import { z, type ZodFormattedError } from "zod";
-import { userSchema } from "@cirrodrive/types";
+import { outputUserDataSchema } from "@cirrodrive/types";
 import { trpc } from "@/shared/api/trpc.ts";
 import { useBoundStore } from "@/shared/store/useBoundStore.ts";
 
@@ -62,7 +62,7 @@ export const useLogin = (opts?: UseLoginOptions): UseLogin => {
   const mutation = trpc.session.login.useMutation({
     ...opts,
     onSuccess: (data, variable, context) => {
-      setUser(userSchema.parse(data));
+      setUser(outputUserDataSchema.parse(data));
       opts?.onSuccess?.(data, variable, context);
     },
     onError: (error, variable, context) => {
