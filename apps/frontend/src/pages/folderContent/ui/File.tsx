f9bb36425d6732b4fe30c5b7254ba921dedbbc8d
ZetabS <songkm2@naver.com>
2024-11-19 21:26:09 +0900

    fix: eslint 에러

---
+++ apps/frontend/src/pages/folderContent/ui/File.tsx
@@ -0,0 +1,11 @@
+import { exampleFiles } from "@/pages/folderContent/ui/FileData.tsx";
+
+interface FileProps {
+  name: string;
+  size: string;
+  type: string;
+}
+
+export const getFiles = (): FileProps[] => {
+  return exampleFiles.map(({ name, size, type }) => ({ name, size, type }));
+};
--- apps/frontend/src/pages/folderContent/ui/FileBox.tsx
+++ apps/frontend/src/pages/folderContent/ui/FileBox.tsx
@@ -1,11 +1,10 @@
-import type React from "react";
 import { FolderClosed, File, Image, MoreVertical } from "lucide-react";
-import { getFiles } from "@/pages/folderContent/ui/file.tsx";
+import { getFiles } from "@/pages/folderContent/ui/File.tsx";
 
-const FileBox: React.FC = () => {
+export function FileBox(): JSX.Element {
   const files = getFiles(); // 파일 이름과 사이즈, 타입 읽기
 
-  const getIcon = (type: string) => {
+  const getIcon = (type: string): JSX.Element => {
     //문서 타입에 따라 아이콘 바뀜
     switch (type) {
       case "image":
@@ -19,9 +18,9 @@ const FileBox: React.FC = () => {
 
   return (
     <div className="mx-auto w-[1350px] border-gray-300">
-      {files.map((file, index) => (
+      {files.map((file) => (
         <div
-          key={index}
+          key={file.name}
           className="flex h-16 items-center justify-between border-b border-gray-200"
         >
           <div className="ml-10">{getIcon(file.type)}</div>
@@ -36,6 +35,4 @@ const FileBox: React.FC = () => {
       ))}
     </div>
   );
-};
-
-export default FileBox;
+}
