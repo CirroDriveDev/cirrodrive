1225ed7e89b3295bd4a0d9ce3fd686c0aa4aae4e
Bear-moon <post120283@gmail.com>
2024-11-27 23:43:26 +0900

    feat: 비회원 업로드 드래그 앤 드롭 (#93)
    * feat: 비회원 드래그앤드롭
    
    -다중 업로드 x
    -뭔가 엄청 허전함
    
    * fix: 업로드 후 정보
    
    -첫 번째 업로드에 파일 이름, 코드 ,링크가 표시 안 되는 걸 변화 감지 시 모달창 나오도록 수정

--- apps/frontend/src/pages/uploadByCode/ui/UploadByCodePage.tsx
+++ apps/frontend/src/pages/uploadByCode/ui/UploadByCodePage.tsx
@@ -1,80 +1,17 @@
-import { File as FileIcon } from "lucide-react";
-import { Link } from "react-router-dom";
 import { Header } from "@/shared/ui/layout/Header.tsx";
 import { Layout } from "@/shared/ui/layout/Layout.tsx";
-import { useUploadPublic } from "@/entities/file/api/useUploadPublic.ts";
-import { LoadingSpinner } from "@/shared/components/LoadingSpinner.tsx";
+import { DragAndDropUpload } from "@/features/folderContent/ui/DragAndDropUpload.tsx";
 
 export function UploadByCodePage(): JSX.Element {
-  const { selectedFile, code, mutation, handleFileChange, handleFormSubmit } =
-    useUploadPublic();
-
-  const generateLink = (c: string): string => {
-    // 현재 오리진에서 링크를 생성
-    return `${window.location.origin}/c/${c}`;
-  };
-
   return (
     // TODO: 반응형 UI 구현
     <Layout header={<Header />}>
       <div className="w-full">
-        <div className="mt-3 flex flex-row text-gray-300">
-          <div className="ml-20 h-[30px] w-[100px] text-center align-middle">
-            Name
-          </div>
-          <div className="ml-[500px] h-[30px] w-[100px] text-center align-middle">
-            Size
-          </div>
-          <div className="ml-[200px] h-[30px] w-[100px] text-center align-middle">
-            Actions
-          </div>
-        </div>
-        <div className="flex h-[520px] items-center justify-center">
+        <div className="flex h-[720px] items-center justify-center">
           <div className="bg-gray-50">
-            <FileIcon size={60} color="gray" />
+            <DragAndDropUpload />
           </div>
         </div>
-        <form method="post" onSubmit={handleFormSubmit}>
-          <div className="flex justify-between bg-gray-400">
-            <div className="w-[90px] bg-gray-300">
-              <Link to="/code" className="ms-6">
-                코드
-              </Link>
-            </div>
-            <div>{selectedFile?.name}</div>
-            <button type="submit" className="w-[100px] bg-gray-300">
-              업로드
-            </button>
-          </div>
-          <div className="flex justify-center">
-            {code ?
-              <>코드: {code}</>
-            : null}
-          </div>
-          <div className="flex justify-center">
-            {code ?
-              <>링크: {generateLink(code)}</>
-            : null}
-          </div>
-          <div className="mt-4 flex h-8 w-full justify-center">
-            {mutation.isPending ?
-              <LoadingSpinner />
-            : null}
-          </div>
-          {mutation.error ?
-            <div className="h-8">
-              <p className="text-red-500">{mutation.error.message}</p>
-            </div>
-          : null}
-          <div className="place-self-center">
-            <input
-              type="file"
-              name="file"
-              className="mt-20"
-              onChange={handleFileChange}
-            />
-          </div>
-        </form>
       </div>
     </Layout>
   );
