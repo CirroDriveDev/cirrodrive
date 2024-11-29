5ffb66a34276ed48f8b7e7fc1bdb4f653b873f6c
ZetabS <songkm2@naver.com>
2024-11-30 00:46:19 +0900

    feat: 전역 모달창 (#107)

--- apps/frontend/src/app/Main.tsx
+++ apps/frontend/src/app/Main.tsx
@@ -2,6 +2,7 @@ import "@/app/index.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "@/app/provider/Provider.tsx";
+import { Modal } from "@/shared/ui/modal/Modal.tsx";
 
 const rootElement = document.getElementById("root");
 
@@ -14,5 +15,6 @@ const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <Provider />
+    <Modal />
   </React.StrictMode>,
 );
