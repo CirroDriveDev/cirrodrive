8de6a1a59fda36f62208fc1ebc918ed76bc8ebcd
Bear-moon <post120283@gmail.com>
2024-12-01 22:40:59 +0900

    feat: 모아보기 페이지 (#111)
    * feat: 문서, 사진 페이지 추가
    
    취짐을 위한 중간 올림
    
    * feat: 회근 파일 추가
    
    테스트를 위해 1분으로 잡았음
    
    * feat: entry.listByUser 추가
    
    * rename: recant 오타 수정
    
    * feat: 문서, 사진 페이지 수정
    
    * feat: revert FolderView.tsx
    
    * feat: 문서 페이지 오타 수정
    
    * feat: 최근 파일
    
    * fix: typeerror
    
    ---------
    
    Co-authored-by: ZetabS <songkm2@naver.com>

---
+++ apps/frontend/src/pages/documents/ui/DocumentsPage.tsx
@@ -0,0 +1,44 @@
+import { useNavigate } from "react-router-dom";
+import type { EntryDTO } from "@cirrodrive/schemas";
+import { useBoundStore } from "@/shared/store/useBoundStore.ts";
+import { SidebarLayout } from "@/shared/ui/SidebarLayout/SidebarLayout.tsx";
+import { Header } from "@/shared/ui/layout/Header.tsx";
+import { Sidebar } from "@/shared/ui/SidebarLayout/Sidebar.tsx";
+import { FolderName } from "@/widgets/folderView/ui/FolderName.tsx";
+import { LoadingSpinner } from "@/shared/components/LoadingSpinner.tsx";
+import { EntryList } from "@/entities/entry/ui/EntryList.tsx";
+import { useEntryByUserList } from "@/entities/entry/api/useEntryListByUser.ts";
+import { inferFileType } from "@/shared/lib/inferFileType.ts";
+
+export function DocumentsPage(): JSX.Element {
+  const navigate = useNavigate();
+  const { user } = useBoundStore();
+  if (user === null) {
+    navigate("/login");
+  }
+
+  const { query: entryListByUserQuery } = useEntryByUserList();
+
+  const filteredEntries: EntryDTO[] =
+    entryListByUserQuery.data?.filter((entry) => {
+      return inferFileType(entry.name) === "text";
+    }) ?? [];
+
+  return (
+    <SidebarLayout
+      header={<Header />} // Pass handler to Header
+      sidebar={<Sidebar />}
+    >
+      <div className="flex w-full flex-grow flex-col items-center">
+        <div className="flex h-16 w-full items-center space-x-4 p-4">
+          <FolderName folderId={null} folderName="문서" />
+        </div>
+        <div className="flex w-full px-4">
+          {entryListByUserQuery.isLoading || !entryListByUserQuery.data ?
+            <LoadingSpinner />
+          : <EntryList entries={filteredEntries} />}
+        </div>
+      </div>
+    </SidebarLayout>
+  );
+}
