72c0081dab52b48643b899dec928498c2416cdde
ZetabS <songkm2@naver.com>
2024-12-01 22:13:16 +0900

    feat: 문서, 사진 페이지 수정

--- apps/frontend/src/pages/photos/ui/PhotosPage.tsx
+++ apps/frontend/src/pages/photos/ui/PhotosPage.tsx
@@ -1,6 +1,14 @@
 import { useNavigate } from "react-router-dom";
+import type { EntryDTO } from "@cirrodrive/schemas";
 import { useBoundStore } from "@/shared/store/useBoundStore.ts";
-import { FolderView } from "@/widgets/folderView/ui/FolderView.tsx";
+import { SidebarLayout } from "@/shared/ui/SidebarLayout/SidebarLayout.tsx";
+import { Header } from "@/shared/ui/layout/Header.tsx";
+import { Sidebar } from "@/shared/ui/SidebarLayout/Sidebar.tsx";
+import { FolderName } from "@/widgets/folderView/ui/FolderName.tsx";
+import { LoadingSpinner } from "@/shared/components/LoadingSpinner.tsx";
+import { EntryList } from "@/entities/entry/ui/EntryList.tsx";
+import { useEntryByUserList } from "@/entities/entry/api/useEntryListByUser.ts";
+import { inferFileType } from "@/shared/lib/inferFileType.ts";
 
 export function PhotosPage(): JSX.Element {
   const navigate = useNavigate();
@@ -9,5 +17,28 @@ export function PhotosPage(): JSX.Element {
     navigate("/login");
   }
 
-  return <FolderView folderId={user!.rootFolderId} type="image" />;
+  const { query: entryListByUserQuery } = useEntryByUserList();
+
+  const filteredEntries: EntryDTO[] =
+    entryListByUserQuery.data?.filter((entry) => {
+      return inferFileType(entry.name) === "image";
+    }) ?? [];
+
+  return (
+    <SidebarLayout
+      header={<Header />} // Pass handler to Header
+      sidebar={<Sidebar />}
+    >
+      <div className="flex w-full flex-grow flex-col items-center">
+        <div className="flex h-16 w-full items-center space-x-4 p-4">
+          <FolderName folderId={null} folderName="사진" />
+        </div>
+        <div className="flex w-full px-4">
+          {entryListByUserQuery.isLoading || !entryListByUserQuery.data ?
+            <LoadingSpinner />
+          : <EntryList entries={filteredEntries} />}
+        </div>
+      </div>
+    </SidebarLayout>
+  );
 }
