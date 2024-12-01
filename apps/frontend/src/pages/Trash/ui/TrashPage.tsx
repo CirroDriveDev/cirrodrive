116b5209952a19749bd8d9ea26eb018db5ee4ed6
Bear-moon <post120283@gmail.com>
2024-12-02 03:44:10 +0900

    feat: 파일 및 폴더 검색 (#110)
    * feat: 검색 추가
    
    자음, 모음 단위 x
    글자 단위 o
    
    * feat: 전역 상태로 검삭어 관리
    
    * feat: 검색 결과 페이지 추가
    
    검색바의 검색을 통해 검색 결과 페이지로 이동
    
    ---------
    
    Co-authored-by: ZetabS <songkm2@naver.com>

--- apps/frontend/src/pages/Trash/ui/TrashPage.tsx
+++ apps/frontend/src/pages/Trash/ui/TrashPage.tsx
@@ -1,19 +1,29 @@
 import { useNavigate } from "react-router-dom";
+import { useTrashEntryList } from "@/entities/entry/api/useTrashEntryList.ts";
 import { Header } from "@/shared/ui/layout/Header.tsx";
 import { Sidebar } from "@/shared/ui/SidebarLayout/Sidebar.tsx";
+import { EntryList } from "@/entities/entry/ui/EntryList.tsx";
 import { SidebarLayout } from "@/shared/ui/SidebarLayout/SidebarLayout.tsx";
 import { useBoundStore } from "@/shared/store/useBoundStore.ts";
 import { LoadingSpinner } from "@/shared/components/LoadingSpinner.tsx";
-import { EntryList } from "@/entities/entry/ui/EntryList.tsx";
-import { useTrashEntryList } from "@/entities/entry/api/useTrashEntryList.ts";
+import { useSearchBarStore } from "@/shared/store/useSearchBarStore.ts";
 
 export function TrashPage(): JSX.Element {
   const navigate = useNavigate();
   const { user } = useBoundStore();
+
   if (user === null) {
     navigate("/login");
   }
+
   const { query: trashEntryListQuery } = useTrashEntryList();
+  const { searchTerm } = useSearchBarStore();
+
+  // 검색어 기준으로 필터링
+  const filteredEntries =
+    trashEntryListQuery.data?.filter((entry) =>
+      entry.name.toLowerCase().includes(searchTerm.toLowerCase()),
+    ) ?? [];
 
   return (
     <SidebarLayout header={<Header />} sidebar={<Sidebar />}>
@@ -21,7 +31,7 @@ export function TrashPage(): JSX.Element {
         <div className="flex w-full px-4">
           {trashEntryListQuery.isLoading || !trashEntryListQuery.data ?
             <LoadingSpinner />
-          : <EntryList entries={trashEntryListQuery.data} />}
+          : <EntryList entries={filteredEntries} />}
         </div>
       </div>
     </SidebarLayout>
