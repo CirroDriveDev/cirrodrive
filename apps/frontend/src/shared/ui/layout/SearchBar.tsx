1b59a73c26c7ad02b2541b65d44f4d962be385c6
ZetabS <songkm2@naver.com>
2024-12-03 00:50:48 +0900

    feat: UI 수정
    다크 모드 리스트, 검색바
    파일 크기 부분 줄바꿈 변경

--- apps/frontend/src/shared/ui/layout/SearchBar.tsx
+++ apps/frontend/src/shared/ui/layout/SearchBar.tsx
@@ -1,14 +1,11 @@
 import { Search } from "lucide-react";
 import { useState, type ChangeEvent, type KeyboardEvent } from "react";
 import { useNavigate } from "react-router-dom";
-import { useTheme } from "@/shared/components/shadcn/ThemeProvider.tsx";
 import { useSearchBarStore } from "@/shared/store/useSearchBarStore.ts";
 
 export function SearchBar(): JSX.Element {
-  const { theme } = useTheme();
   const { searchTerm, setSearchTerm } = useSearchBarStore();
   const navigate = useNavigate();
-  const foregroundColor = theme === "light" ? "black" : "white";
   const outlineClassList =
     "outline outline-1 outline-ring hover:outline-foreground";
   const [classList, setClassList] = useState<string>(outlineClassList);
@@ -33,15 +30,15 @@ export function SearchBar(): JSX.Element {
 
   return (
     <div
-      className={`h-10 max-w-96 flex-grow rounded-md bg-background ${classList}`}
+      className={`h-10 max-w-96 flex-grow rounded-md bg-white text-black ${classList}`}
     >
       <label className="flex space-x-2 p-2">
-        <Search color={foregroundColor} />
+        <Search />
         <input
           type="text"
           placeholder="검색"
           value={searchTerm}
-          className="flex-grow bg-background text-foreground focus:outline-none"
+          className="flex-grow bg-white focus:outline-none"
           onFocus={handleFocus}
           onBlur={handleBlur}
           onChange={handleInputChange} // 검색어 보내기
