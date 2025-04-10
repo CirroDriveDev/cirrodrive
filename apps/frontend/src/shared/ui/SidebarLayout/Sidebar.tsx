f4add9b77c04074bfcbe8fd2e1b61e852cebef46
yoo-j-h <gusdlcjstk10@gmail.com>
2025-04-10 18:49:49 +0900

    feat : 관리자페이지 ui

--- apps/frontend/src/shared/ui/SidebarLayout/Sidebar.tsx
+++ apps/frontend/src/shared/ui/SidebarLayout/Sidebar.tsx
@@ -1,15 +1,18 @@
+import { useLocation } from "react-router-dom";
 import { useBoundStore } from "@/shared/store/useBoundStore.ts";
 import { PublicSidebar } from "@/shared/ui/SidebarLayout/PublicSidebar.tsx";
 import { MemberSidebar } from "@/shared/ui/SidebarLayout/MemberSidebar.tsx";
+import { AdminSidebar } from "@/shared/ui/SidebarLayout/AdminSidebar.tsx";
 
 export function Sidebar(): JSX.Element {
   const { user } = useBoundStore();
+  const location = useLocation();
 
-  return (
-    <>
-      {user ?
-        <MemberSidebar />
-      : <PublicSidebar />}
-    </>
-  );
+  const isAdminPage = location.pathname.startsWith("/admin");
+
+  if (isAdminPage) {
+    return <AdminSidebar />;
+  }
+
+  return user ? <MemberSidebar /> : <PublicSidebar />;
 }
