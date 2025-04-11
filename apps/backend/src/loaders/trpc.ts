ff2a304fc64ac7c9521f8185682738f1e86b8182
ZetabS <songkm2@naver.com>
2025-04-11 22:26:17 +0900

    fix: middleware 중복 선언 제거

--- apps/backend/src/loaders/trpc.ts
+++ apps/backend/src/loaders/trpc.ts
@@ -58,7 +58,6 @@ const t = initTRPC.context<Context>().create({
 export const middleware = t.middleware;
 export const router = t.router;
 export const procedure = t.procedure;
-export const middleware = t.middleware;
 
 export const authedProcedure = procedure.use(async (opts) => {
   const { ctx } = opts;
