b4673c0ae767761a59311f49a05ea10fa165ec70
ZetabS <songkm2@naver.com>
2025-04-14 03:10:38 +0900

    [TSK-204] fix: 파일 업로드 시 S3 버킷에 업로드하지 않는 오류 수정 (#198)
    * feat: 키 생성 방식 변경
    
    * fix: useUpload의 upload를 완전한 async 함수로 수정
    
    * fix: 에러가 발생했을 때에만 에러 메시지 출력
    
    * fix: 에러가 발생했을 때에만 에러 메시지 출력

--- apps/frontend/src/features/fileUpload/ui/FileUploadDropzoneOverlay.tsx
+++ apps/frontend/src/features/fileUpload/ui/FileUploadDropzoneOverlay.tsx
@@ -16,7 +16,7 @@ export function FileUploadDropzoneOverlay({
   const [dragOver, setDragOver] = useState(false);
   const { openModal } = useBoundStore();
 
-  const { upload } = useUpload();
+  const { upload, error: uploadError } = useUpload();
 
   // 드래그 앤 드롭 공통 처리
   const handleDrop = async (
@@ -30,21 +30,21 @@ export function FileUploadDropzoneOverlay({
     if (files?.[0]) {
       const file = files[0];
 
-      await upload(file, folderId, {
-        onSuccess: (data) => {
-          // 업로드 성공 시 추가 작업 호출
-          if (onUploadSuccess) onUploadSuccess();
-          openModal({
-            title: "업로드 성공",
-            content: UploadSuccessModal(file.name, data.code),
-          });
-        },
-        onError: (error) => {
-          openModal({
-            title: "업로드 실패",
-            content: <div>{error.message}</div>,
-          });
-        },
+      const { code } = await upload(file, folderId);
+      if (uploadError) {
+        openModal({
+          title: "업로드 실패",
+          content: <div>{uploadError?.message}</div>,
+        });
+        return;
+      }
+
+      if (onUploadSuccess) {
+        onUploadSuccess();
+      }
+      openModal({
+        title: "업로드 성공",
+        content: UploadSuccessModal(file.name, code),
       });
     }
   };
