ca832c65d68e45264c8be8524d4fcbe1621816a0
Bear-moon <post120283@gmail.com>
2025-04-17 23:16:47 +0900

    feat: 통계 대시보드 구현_통계 카드 Ui 구현(mock 데이터)
    commit 1aa9a391f8e231fb987b07ab362dd8a5ebd7a78a
    Author: Bear-moon <post120283@gmail.com>
    Date:   Sun Apr 13 02:35:11 2025 +0900
    
        feat: 통계 대시보드 구현_통계 카드 Ui 구현
    
        mock 데이터 활용

--- apps/frontend/src/pages/admin/ui/AdminDashboardPage.tsx
+++ apps/frontend/src/pages/admin/ui/AdminDashboardPage.tsx
@@ -4,6 +4,7 @@ import { SidebarLayout } from "@/shared/ui/SidebarLayout/SidebarLayout.tsx";
 import { WeeklyActivityChart } from "@/features/statistics/ui/WeeklyActivityChart.tsx";
 import { MonthlyActivityChart } from "@/features/statistics/ui/MonthlyActivityChart.tsx";
 import { RecentUploadedFiles } from "@/features/folderContent/ui/statistics/ui/RecentUploadedFiles.tsx";
+import { KpiOverview } from "@/features/statistics/ui/KpiOverview.tsx";
 
 // 관리자 대시보드 메인 페이지
 export function AdminDashboardPage(): JSX.Element {
@@ -12,6 +13,7 @@ export function AdminDashboardPage(): JSX.Element {
       <div className="flex w-full flex-grow flex-col items-center">
         <div className="w-full max-w-screen-xl space-y-6 px-6 py-4">
           {/* 카드 */}
+          <KpiOverview />
 
           {/* 최근 업로드된 파일 리스트 */}
           <RecentUploadedFiles />
