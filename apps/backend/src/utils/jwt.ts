8a78eed5adb5b1975fd248faeafdeb884ebf65ea
ZetabS <songkm2@naver.com>
2025-04-18 20:21:40 +0900

    chore: 환경변수 관련 누락된 변경사항 적용
    일부 주석 수정
    vite-env.d.ts에서 타이핑 제거

--- apps/backend/src/utils/jwt.ts
+++ apps/backend/src/utils/jwt.ts
@@ -5,7 +5,7 @@ import {
 import { env } from "@/loaders/env.ts";
 
 /**
- * JWT 비밀키를 생성합니다. 환경 변수 `VITE_JWT_SECRET`에서 비밀키를 가져오며, 없을 경우 기본값을 사용합니다.
+ * JWT 비밀키를 생성합니다. 환경 변수 `JWT_SECRET`에서 비밀키를 가져오며, 없을 경우 기본값을 사용합니다.
  *
  * @returns KeyObject - 암호화 키 객체
  */
--- apps/backend/src/vite-env.d.ts
+++ apps/backend/src/vite-env.d.ts
@@ -1,13 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_EC2_PUBLIC_URL: string;
-  readonly VITE_CLIENT_PORT: string;
-  readonly VITE_SERVER_PORT: string;
-  readonly VITE_AWS_REGION: string;
-  readonly VITE_AWS_S3_BUCKET: string;
-  readonly VITE_SES_SOURCE_EMAIL: string;
-  readonly VITE_JWT_SECRET: string;
+  readonly DEPRECATED: undefined;
 }
 
 interface ImportMeta {
