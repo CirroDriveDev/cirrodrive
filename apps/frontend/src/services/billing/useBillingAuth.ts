59ab602e023a8e3ec3acb66471cc4a392d24cbaa
ZetabS <songkm2@naver.com>
2025-05-16 23:45:54 +0900

    feat(프론트엔드): useBillingAuth 훅 구현

--- apps/frontend/src/services/billing/useBillingAuth.ts
+++ apps/frontend/src/services/billing/useBillingAuth.ts
@@ -1,12 +1,30 @@
 import { useState } from "react";
+import { tossPayments } from "@/services/billing/toss.ts";
+import { useBoundStore } from "@/store/useBoundStore.ts";
 
 export function useBillingAuth(successUrl: string, failUrl: string) {
   const [isLoading, setIsLoading] = useState(false);
+  const { user } = useBoundStore();
+
   return {
-    requestBillingAuth: () => {
-      return new Promise<void>(() => {
-        throw new Error("Not implemented");
-      });
+    requestBillingAuth: async (): Promise<void> => {
+      if (!user) {
+        throw new Error("User is not logged in");
+      }
+      const payment = tossPayments.payment({ customerKey: user.id });
+
+      setIsLoading(true);
+      try {
+        await payment.requestBillingAuth({
+          method: "CARD",
+          successUrl,
+          failUrl,
+          customerName: user.username,
+          customerEmail: user.email,
+        });
+      } finally {
+        setIsLoading(false);
+      }
     },
     isLoading,
   };
