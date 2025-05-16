5e60f82f50b710d797eb9d7cee6105a9b498e381
ZetabS <songkm2@naver.com>
2025-05-16 23:43:06 +0900

    Revert "feat(프론트엔): usesBillingAuth에 customerKey 매개변수 추가"
    This reverts commit e587e10f29fa032f8361ac42c2e7665287c009ff.

--- apps/frontend/src/services/billing/useBillingAuth.ts
+++ apps/frontend/src/services/billing/useBillingAuth.ts
@@ -3,7 +3,7 @@ import { useState } from "react";
 export function useBillingAuth(successUrl: string, failUrl: string) {
   const [isLoading, setIsLoading] = useState(false);
   return {
-    requestBillingAuth: (customerKey: string) => {
+    requestBillingAuth: () => {
       return new Promise<void>(() => {
         throw new Error("Not implemented");
       });
