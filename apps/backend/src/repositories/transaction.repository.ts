d654f1596a41593217ece0bea3d2f2eba98b0603
ZetabS <songkm2@naver.com>
2025-05-17 22:14:27 +0900

    feat: transaction 리포지토리로 수정

---
+++ apps/backend/src/repositories/transaction.repository.ts
@@ -0,0 +1,34 @@
+import { injectable } from "inversify";
+import { Prisma, Transaction, $Enums } from "@cirrodrive/database";
+import { BaseRepository } from "@/repositories/base.repository.ts";
+
+@injectable()
+export class TransactionRepository extends BaseRepository {
+  // Create
+  async create(
+    data: Prisma.TransactionUncheckedCreateInput,
+  ): Promise<Transaction> {
+    return this.prisma.transaction.create({ data });
+  }
+
+  // Read
+  async findById(id: string): Promise<Transaction | null> {
+    return this.prisma.transaction.findUnique({ where: { id } });
+  }
+  async listBySubscriptionId(subscriptionId: string): Promise<Transaction[]> {
+    return this.prisma.transaction.findMany({ where: { subscriptionId } });
+  }
+
+  // Update
+  async updateStatusById(
+    id: string,
+    status: $Enums.TransactionStatus,
+  ): Promise<Transaction> {
+    return this.prisma.transaction.update({ where: { id }, data: { status } });
+  }
+
+  // Delete
+  async deleteById(id: string): Promise<Transaction> {
+    return this.prisma.transaction.delete({ where: { id } });
+  }
+}
