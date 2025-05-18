2ba84cc428efb0db3c28171f313c04f1b480f264
ZetabS <songkm2@naver.com>
2025-05-19 06:41:17 +0900

    feat(shadcn): 컴포넌트 전체 설치

---
+++ apps/frontend/src/shadcn/components/aspect-ratio.tsx
@@ -0,0 +1,6 @@
+/* eslint-disable -- shadcn */
+import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
+
+const AspectRatio = AspectRatioPrimitive.Root;
+
+export { AspectRatio };
--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -3,6 +3,7 @@ import webConfig from "@cirrodrive/tailwind-config/web";
 
 // eslint-disable-next-line import/no-default-export -- Tailwind CSS requires default export
 export default {
+  darkMode: ["class"],
   content: ["./src/**/*.{ts,tsx}"],
   presets: [webConfig],
   theme: {
@@ -19,12 +20,46 @@ export default {
         "nanum-square-acr": ['"NanumSquareAcr"', "sans-serif"],
       },
       fontWeight: {
-        400: "400",
-        500: "500",
-        600: "600",
-        700: "700",
-        800: "800",
-        900: "900",
+        "400": "400",
+        "500": "500",
+        "600": "600",
+        "700": "700",
+        "800": "800",
+        "900": "900",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      colors: {
+        sidebar: {
+          "DEFAULT": "hsl(var(--sidebar-background))",
+          "foreground": "hsl(var(--sidebar-foreground))",
+          "primary": "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          "accent": "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          "border": "hsl(var(--sidebar-border))",
+          "ring": "hsl(var(--sidebar-ring))",
+        },
       },
     },
   },
