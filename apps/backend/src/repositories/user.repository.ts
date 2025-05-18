f5087ebb55b6127ce780c83acda490aba29cbba1
ZetabS <songkm2@naver.com>
2025-05-19 03:06:12 +0900

    feat: UserRepository 추가

---
+++ apps/backend/src/repositories/user.repository.ts
@@ -0,0 +1,61 @@
+import { injectable } from "inversify";
+import { Prisma, User, Plan } from "@cirrodrive/database";
+import { BaseRepository } from "@/repositories/base.repository.ts";
+import { DBNotFoundError } from "@/errors/db-error-classes.ts";
+
+@injectable()
+export class UserRepository extends BaseRepository {
+  // Create
+  public async create(data: Prisma.UserUncheckedCreateInput): Promise<User> {
+    return this.prisma.user.create({ data });
+  }
+
+  // Read
+  public async getById(id: string): Promise<User> {
+    const result = await this.prisma.user.findUnique({ where: { id } });
+    if (!result) {
+      throw new DBNotFoundError("User", { id });
+    }
+    return result;
+  }
+
+  public async getByUsername(username: string): Promise<User> {
+    const result = await this.prisma.user.findUnique({ where: { username } });
+    if (!result) {
+      throw new DBNotFoundError("User (username)", { username });
+    }
+    return result;
+  }
+
+  public async getByEmail(email: string): Promise<User> {
+    const result = await this.prisma.user.findUnique({ where: { email } });
+    if (!result) {
+      throw new DBNotFoundError("User (email)", { email });
+    }
+    return result;
+  }
+
+  public async getCurrentPlanByUserId(userId: string): Promise<Plan> {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+      include: { currentPlan: true },
+    });
+    if (!user?.currentPlan) {
+      throw new DBNotFoundError("User's current plan", { userId });
+    }
+    return user.currentPlan;
+  }
+
+  // Update
+  public async updateById(
+    id: string,
+    data: Prisma.UserUpdateInput,
+  ): Promise<User> {
+    return this.prisma.user.update({ where: { id }, data });
+  }
+
+  // Delete
+  public async deleteById(id: string): Promise<User> {
+    return this.prisma.user.delete({ where: { id } });
+  }
+}
