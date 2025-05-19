91d77d32c802f470428f2ffcc3ebc7072cb30d0f
ZetabS <songkm2@naver.com>
2025-05-19 11:59:55 +0900

    fix: 탑 레벨 await 제거

--- apps/frontend/src/services/billing/toss.ts
+++ apps/frontend/src/services/billing/toss.ts
@@ -1,5 +1,2 @@
 // 클라이언트 키는 하드코딩해도 보안상 문제없습니다.
-import { loadTossPayments } from "@tosspayments/tosspayments-sdk";
-
-const CLIENT_KEY = "test_ck_d46qopOB892JAe2jwoQY3ZmM75y0";
-export const tossPayments = await loadTossPayments(CLIENT_KEY);
+export const TOSS_CLIENT_KEY = "test_ck_d46qopOB892JAe2jwoQY3ZmM75y0";
--- apps/frontend/src/services/billing/useBillingAuth.ts
+++ apps/frontend/src/services/billing/useBillingAuth.ts
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { tossPayments } from "@/services/billing/toss.ts";
+import { loadTossPayments } from "@tosspayments/tosspayments-sdk";
 import { useBoundStore } from "@/store/useBoundStore.ts";
+import { TOSS_CLIENT_KEY } from "@/services/billing/toss.ts";
 
 export function useBillingAuth(successUrl: string, failUrl: string) {
   const [isLoading, setIsLoading] = useState(false);
@@ -11,6 +12,7 @@ export function useBillingAuth(successUrl: string, failUrl: string) {
       if (!user) {
         throw new Error("User is not logged in");
       }
+      const tossPayments = await loadTossPayments(TOSS_CLIENT_KEY);
       const payment = tossPayments.payment({ customerKey: user.id });
 
       setIsLoading(true);
