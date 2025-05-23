a1d1725eb951350c6ad90d1bc81dc8ac88ab0f9a
lirafy <lirafy908@gmail.com>
2025-05-23 23:33:13 +0900

    fileService Presigned POST 생성 메서드 구현
    S3 Presigned POST URL을 생성하는 fileService 메서드를 구현

--- apps/backend/src/services/file.upload.service.ts
+++ apps/backend/src/services/file.upload.service.ts
@@ -1,10 +1,33 @@
-import { type fileMetadataDTOSchema } from "@cirrodrive/schemas";
-import { type z } from "zod";
+// src/services/file.upload.service.ts
+import { inject, injectable } from "inversify";
+import { z } from "zod";
+import { fileMetadataDTOSchema, s3PresignedPostSchema } from "@cirrodrive/schemas";
+import { S3Service, S3_KEY_PREFIX } from "@/services/s3.service.ts";
 
-  export class FileService {
-    async saveFileMetadata(_metadata: z.infer<typeof fileMetadataDTOSchema>): Promise<void> {
+const MAX_POST_FILE_SIZE = 1024 * 1024 * 1024; // 1GB
+
+@injectable()
+export class FileService {
+  constructor(
+    @inject(S3Service) private readonly s3Service: S3Service,
+  ) {}
+
+  async generatePresignedPost(fileName: string, fileType: string) {
+    const prefix = S3_KEY_PREFIX.PUBLIC_UPLOADS;
+    const key = this.s3Service.generateS3ObjectKey(prefix, fileName);
+
+    const presignedPost = await this.s3Service.generatePresignedPost({
+      key,
+      contentType: fileType,
+      expires: 120,
+      maxSizeInBytes: MAX_POST_FILE_SIZE,
+    });
+
+    return s3PresignedPostSchema.parse(presignedPost);
+  }
+
+  async saveFileMetadata(_metadata: z.infer<typeof fileMetadataDTOSchema>): Promise<void> {
   // TODO: 실제 구현 예정
-  
-}
+  }
 
 }
