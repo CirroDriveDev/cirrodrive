ba079823bae08d67af7a8755620c3ae07f2ca994
ZetabS <songkm2@naver.com>
2025-06-07 04:47:27 +0900

    test: database 패키지 테스트 삭제

--- packages/database/test/client.test.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "#client";
-
-describe("Prisma Client", () => {
-  test("데이터베이스에 연결할 수 있어야 한다.", async () => {
-    const result = await prisma.$executeRaw`SELECT 1 + 1 AS result`;
-    expect(result).toBeDefined();
-  });
-});
